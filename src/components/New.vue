66b55a8656f724bad9976024cb8033d85072fb8c
Juan Munoz <juan.munoz@lemon42.com>
2017-05-29 00:13:30 -0700

    Component behavior

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     <h3>New Quote</h3>
+    <hr>
+    <button @click="counter++">New</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
 
   export default {
-
+      data: function() {
+        return {
+          counter: 0
+        };
+      },
+      destroyed(){
+        console.log("New was destroyed");
+      }
   }
 </script>
 
