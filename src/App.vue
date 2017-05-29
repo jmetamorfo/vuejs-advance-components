864b6fb42ee7429905a601fb88f7d359d3f7e539
Juan Munoz <juan.munoz@lemon42.com>
2017-05-29 00:07:27 -0700

    Add dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
-              <app-quote>
-                <h1 slot="title">{{ quoteTitle }}</h1>
+          <div class="col-xs-12">
+            <button @click="selectedComponent='appQuote'">Quote</button>
+            <button @click="selectedComponent='appAuthor'">Author</button>
+            <button @click="selectedComponent='appNew'">New</button>
+            <hr>
+            <p>{{ selectedComponent }}</p>
+            <hr>
+            <component :is="selectedComponent">
+              <p>Default content</p>
+            </component>
+            <!--            <app-quote>
+                            <h1 slot="title">{{ quoteTitle }}</h1>
                 <p slot="content">A wonderful Quote!!! using slots</p>
-              </app-quote>
-            </div>
+                          </app-quote>-->
+          </div>
         </div>
     </div>
 </template>
 
 <script>
   import Quote from './components/Quote.vue'
+  import Author from './components/Author.vue'
+  import New from './components/New.vue'
 
     export default {
         data: function() {
           return {
-            quoteTitle: 'The Quote'
+            quoteTitle: 'The Quote',
+            selectedComponent: 'appQuote'
           };
         },
         components: {
-          'app-quote': Quote
+           appQuote: Quote,
+           appAuthor: Author,
+           appNew: New
         }
     }
 </script>
